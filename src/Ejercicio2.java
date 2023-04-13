7885889aa10e5088405fc4d706d4f89da88789c1
PaolaRamosGutierrez <paolagutierrezramos5@gmail.com>
2023-04-13 18:17:25 -0400

    add exercises 2,7,8

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,11 +1,26 @@
 import java.util.ArrayList;
-
+import enums.Gender;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 public class Ejercicio2 {
     public static void main(String[] args) {
-
+    ArrayList<Person> personList = GenerateData.getPersons();
+    countMenAndWomenWithoutRepeat(personList);
+}
+    private static void countMenAndWomenWithoutRepeat(ArrayList<Person> personList){
+        Map<Gender, Long> totalCount = personList.stream()
+                .filter(distinctByKey(Person::getEmail))
+                .collect(Collectors.groupingBy(Person::getGender, Collectors.counting()));
+        System.out.println(totalCount);
     }
-    private static void filterPersonsByAgeAndShort(ArrayList<Person> personList){
 
+    private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        return t -> seen.add(keyExtractor.apply(t));
     }
 }
 
--- src/Ejercicio8.java
+++ src/Ejercicio8.java
@@ -1,2 +1,46 @@
+import enums.Gender;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
 public class Ejercicio8 {
+    public static void main(String[] args) {
+        ArrayList<Person> personList = GenerateData.getPersons();
+        filterPeopleByGenderDateOfBirthCell(personList);
+    }
+    private static void filterPeopleByGenderDateOfBirthCell(ArrayList<Person> personList){
+        Scanner scanner = new Scanner(System.in);
+        int selection;
+        do {
+            System.out.println("Seleccione el género:");
+            System.out.println("1 - Femenino");
+            System.out.println("2 - Masculino");
+            System.out.println("0 - Salir");
+
+            selection = scanner.nextInt();
+
+            switch (selection) {
+                case 1:
+                    System.out.println("Ha seleccionado el género Femenino.");
+                    break;
+                case 2:
+                    System.out.println("Ha seleccionado el género Masculino.");
+                    break;
+                case 0:
+                    System.out.println("Fin del programa");
+                    break;
+                default:
+                    System.out.println("Selección inválida. Intente de nuevo.");
+                    break;
+            }
+       Gender gender = (selection == 1) ? Gender.FEMALE : selection == 2 ? Gender.MALE : null;
+       personList.stream()
+                .distinct()
+                .sorted(Comparator.comparing(Person::getBirthDate).reversed())
+                .filter(p->p.getPhone()!=null)
+                .filter(p->p.getGender()==gender)
+                .limit(20)
+                .forEach(System.out::println);
+        } while (selection != 0);
+        scanner.close();
+    }
 }
