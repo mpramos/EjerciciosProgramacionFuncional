6984c0a1a73202beb761d0d34f52eef13eebedec
PaolaRamosGutierrez <paolagutierrezramos5@gmail.com>
2023-04-13 15:05:53 -0400

    enumeracion de ejercicios

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class Ejercicio2 {
+    public static void main(String[] args) {
+
+    }
+    private static void filterPersonsByAgeAndShort(ArrayList<Person> personList){
+
+    }
 }
+
